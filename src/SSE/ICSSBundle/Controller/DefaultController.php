82de6dc8a10cc31d7f1adbe1d614995246be3482
Breezewish <me@breeswish.org>
2015-07-13 17:49:01 +0800

    appending style

--- src/SSE/ICSSBundle/Controller/DefaultController.php
+++ src/SSE/ICSSBundle/Controller/DefaultController.php
@@ -5,14 +5,23 @@ namespace SSE\ICSSBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
+     * @Route("/", name="index")
+     */
+    public function indexAction()
+    {
+        return new Response('<body><h1>It works!</h1></body>');
+    }
+
+    /**
      * @Route("/hello/{name}")
      * @Template()
      */
-    public function indexAction($name)
+    public function helloAction($name)
     {
         return array('name' => $name);
     }
