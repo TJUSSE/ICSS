03dd6cb67ba26b4e25c550e42f053971f2b097ba
maerye <350046243@qq.com>
2015-07-16 11:52:47 +0800

    company calss

--- src/SSE/ICSSBundle/Entity/Company.php
+++ src/SSE/ICSSBundle/Entity/Company.php
@@ -56,7 +56,41 @@ class Company
      */
     private $id;
 
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     *
+     * @ORM\ManyToMany(targetEntity="SSE\ICSSBundle\Entity\CompanyClass")
+     * @ORM\JoinTable(name="company_with_class",
+     *   joinColumns={
+     *     @ORM\JoinColumn(name="company_id", referencedColumnName="id")
+     *   },
+     *   inverseJoinColumns={
+     *     @ORM\JoinColumn(name="class_id", referencedColumnName="id")
+     *   }
+     * )
+     */
+    private $class;
 
+    /**
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getClass()
+    {
+        return $this->class;
+    }
+
+    /**
+     * Add class
+     *
+     * @param \SSE\ICSSBundle\Entity\CompanyClass $class
+     * @return Company
+     */
+    public function addClass(\SSE\ICSSBundle\Entity\CompanyClass $class)
+    {
+        $this->class[] = $class;
+
+        return $this;
+    }
     /**
      * Set name
      *
@@ -180,5 +214,10 @@ class Company
     public function getId()
     {
         return $this->id;
+    }
+
+    public function __toString()
+    {
+        return $this->getName() ?: '企业';
     }
 }
